1cc0147a263f35227f1961c3cd08a0f09743d85d
Yassine Zenati <yassine.zenati@icloud.com>
2023-10-27 15:12:07 +0200

    * chore(deploy.template.yaml): add command to run database migration and seeding after building the project * feat(.env.example): update database configuration to use MySQL and provide credentials * docs(README.md): add Railway deployment button * feat(DatabaseSeeder.php): create admin user if it doesn't exist during database seeding * feat(railway.yml): add Railway deployment configuration with MySQL plugin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        if (!User::exists()) {
+            User::create([
+                'name' => 'Admin',
+                'email' => 'admin@marmotte.io',
+                'password' => Hash::make('marmotte.io'),
+            ]);
+        }
     }
 }
