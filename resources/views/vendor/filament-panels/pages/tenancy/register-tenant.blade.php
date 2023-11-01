75aff98a5f142b2694cce59ec759164aa694afc8
Yassine Zenati <yassine.zenati@icloud.com>
2023-11-01 17:29:39 +0100

    chore(register-tenant.blade.php): add explanatory text to the registration form to guide users in filling out the default team name field

--- resources/views/vendor/filament-panels/pages/tenancy/register-tenant.blade.php
+++ resources/views/vendor/filament-panels/pages/tenancy/register-tenant.blade.php
@@ -1,5 +1,6 @@
 <x-filament-panels::page.simple>
     <x-filament-panels::form wire:submit="register">
+        <p class="text-md p-1 text-gray-400">To proceed, please fill out the default team name in the form provided below.</p>
         {{ $this->form }}
 
         <x-filament-panels::form.actions
