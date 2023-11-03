1079e606ea5bdb25167ab18f81c981515520e4db
Yassine Zenati <yassine.zenati@icloud.com>
2023-11-03 16:38:31 +0100

    chore(simple.blade.php): update simple page layout to include logo image and adjust styling

--- resources/views/vendor/filament-panels/components/page/simple.blade.php
+++ resources/views/vendor/filament-panels/components/page/simple.blade.php
@@ -4,7 +4,8 @@
 ])
 
 <div {{ $attributes->class(['fi-simple-page', 'w-full',]) }} style="padding-left: 3rem; padding-right: 3rem;">
-    <h1 class="text-2xl font-bold text-center mb-5">Marmotte.io</h1>
+    <img src="{{ asset('logo.svg') }}" width="90%" />
+    <br/>
     <br/>
     <section class="grid auto-cols-fr gap-y-6">
         {{ $slot }}
