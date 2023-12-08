32800ea5e892ad13e6e0bc1ce8eb53395654c4b8
Yassine Zenati <yassine.zenati@icloud.com>
2023-12-08 07:32:08 +0100

    Fix: https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
-            \URL::forceScheme('https');
-        }
     }
 }
