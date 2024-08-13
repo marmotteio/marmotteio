50eb05448876bb21f2869a2d8781bb855938d7ff
Yassine Zenati <yassine.zenati@icloud.com>
2024-08-13 21:09:15 +0200

    updates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
+use App\Models\Team;
+use App\Models\HardwareStatus;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -14,12 +16,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $team = Team::create([
+            'name' => 'Team 1',
+        ]);
+
         if (! User::exists()) {
-            User::create([
+            $user = User::create([
                 'name' => 'Admin',
                 'email' => 'admin@marmotte.io',
                 'password' => Hash::make('marmotte.io'),
             ]);
+
+            $team->users()->attach($user);
+        }
+
+        $hardwareStatusesNames = [
+            'Deployed',
+            'In Stock',
+            'In Repair',
+            'Retired',
+            'Lost/Stolen',
+        ];
+
+        foreach ($hardwareStatusesNames as $statusName) {
+            HardwareStatus::create(['team_id' => $team->id, 'name' => $statusName]);
         }
     }
 }
